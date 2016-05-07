e71ca384a391121c01a962bc1370007ac3972020
Diego_93 <dievazqu@itba.edu.ar>
2016-05-06 23:21:17 -0300

    optimizacion de codigo (removido un par de for innecesarios) y fixeo de un bug

--- learning_network_tanh.m
+++ learning_network_tanh.m
@@ -7,34 +7,26 @@ function net = learning_network_tanh(network, learning_rate, in, out, beta)
 	%% el valor que sale de la capa m, i es el numero del patron y j es el numero del nodo
 	for i=1 : M
         aux = [ ones(size(aux)(1), 1)*-1 aux]; %%le agrego el umbral
-		h{i} = aux*network{i}; %%hago pasar el patron, por la capa i
+		h{i} = aux*network{i}; %%hago pasar el patron por la capa i
         aux = g(beta*h{i}); %%pasamos la h por la funcion de activacion
+        v{i} = aux; %% g(h_i)
     end
 	network_out = aux; 
-	
-	%%ACA ESTA LA PAPA 
-
 
 	%% solo para g = tanh
 	%%calculo los delta
 	%%ultima capa:
-	for i=1 : size(network{M})(2)
-		delta{M}(:,i) = beta*(1-(tanh(beta*h{M}(:,i)).**2)).*(out(:,i)-network_out(:,i));
-	end
+	delta{M} = beta*(1-v{M}.**2).*(out-network_out);
 	%%capas intermedias:
     for m=M : -1 : 2
-		for i=1 : size(network{m-1})(2)
-			delta{m-1}(:,i) = beta*(1-(tanh(beta*h{m-1}(:,i)).**2)).*(delta{m}*network{m}(i,:)');
-		end
+		delta{m-1} = beta*(1-v{m-1}.**2).*(delta{m}*network{m}(2:end,:)'); %%preguntar a Cristina
 	end
 	
-	w=network;
-	w{1} = w{1} + learning_rate*([ones(size(aux)(1), 1)*-1 g(beta*in)]'*delta{1});
-	
+	network{1} = network{1} + learning_rate*([ones(size(aux)(1), 1)*-1 in]'*delta{1});
 	for m=2 : M
-		w{m} = w{m} + learning_rate*([ones(size(in)(1), 1)*-1 g(beta*h{m-1})]'*delta{m});	
+		network{m} = network{m} + learning_rate*([ones(size(in)(1), 1)*-1 v{m-1}]'*delta{m});	
 	end
-	net = w;
+	net = network;
 end
 
 
